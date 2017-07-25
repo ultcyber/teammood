07c8be97e5672667a3953f34a2c0b8d035b71b9d
Ultcyber <sedov00@gmail.com>
2017-07-25 18:28:42 +0200

    Simple view

--- MoodApp/mood/views.py
+++ MoodApp/mood/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from mood.models import Mood
 from django.shortcuts import render
 from django.http import HttpResponse
+from .utils.token_generator import Token
 
 def sendMood(request, token):
-    html = "<b> Your token is {}<b>".format(token)
+    random_token = Token().getToken()
+    html = "<b> Your token is {}<b>. I generated a random token = {}".format(token, random_token)
     return HttpResponse(content=html, status=201)
